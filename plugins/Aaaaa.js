c9396e67686b13f8283acf1c9230c0bf1170fb36
BOTGATITO <149286603+BOTGATITO@users.noreply.github.com>
2024-05-04 23:33:17 -0500

    Update Aaaaa.js

--- plugins/Aaaaa.js
+++ plugins/Aaaaa.js
@@ -1,5 +1,5 @@
 let handler = async (m, { conn, usedPrefix, isOwner }) => {
-let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:creador 🇦🇱\nFN:creadorl 🇦🇱\nORG:creador 🇦🇱\nTITLE:\nitem1.TEL;waid=593993370003:593993370003\nitem1.X-ABLabel: Jeffo 🇦🇱\nX-WA-BIZ-DESCRIPTION:\nX-WA-BIZ-NAME:Daniel 🇦🇱U+2069\nEND:VCARD`
+let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:creador 🇦🇱\nFN:CREADOR 👤\nORG:creador 🇦🇱\nTITLE:\nitem1.TEL;waid=593979133620:593979133620\nitem1.X-ABLabel: Jeffo 🇦🇱\nX-WA-BIZ-DESCRIPTION:\nX-WA-BIZ-NAME:Daniel 🇦🇱U+2069\nEND:VCARD`
 await conn.sendMessage(m.chat, { contacts: { displayName: 'Creador', contacts: [{ vcard }] }}, {quoted: m})
 }
 handler.help = ['owner']
